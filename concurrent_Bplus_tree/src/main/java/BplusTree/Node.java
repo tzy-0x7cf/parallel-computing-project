e7922326ea30f3ef87a894b1aaa177edd2e81627
tzy <tangzhiyan.tzy@alibaba-inc.com>
2022-07-20 15:20:10 +0800

    feat : Node and internalNode interface design

---
+++ concurrent_Bplus_tree/src/main/java/BplusTree/Node.java
@@ -0,0 +1,70 @@
+package BplusTree;
+
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Setter
+public abstract class Node <K extends Comparable, V>{
+    protected static int numKeysPerNode = 8;
+
+    protected int numKeys;
+    protected List<K> keys;
+    protected Node<K,V> parent = null;
+    protected Node<K,V> next = null;
+    protected Lock lock = null;
+    protected boolean isLeaf;
+
+    public Node (K key){
+        numKeys = 1;
+        keys = new ArrayList<>();
+        keys.set(0,key);
+        lock = new ReentrantLock();
+    }
+
+    public Node(List<K> keys,int numKeys,Node<K,V> next, Node<K,V> parent){
+        this.keys = keys;
+        this.numKeys = numKeys;
+        this.next = next;
+        this.parent = parent;
+        lock = new ReentrantLock();
+    }
+
+    public int numKeys(){
+        return numKeys;
+    }
+
+    public void sort(){
+        Collections.sort(keys);
+    }
+
+    public K LowKey(){
+        return keys.get(0);
+    }
+
+    public K UpKey(){
+        return keys.get(numKeys - 1);
+    }
+
+    /**
+     * Obtains a lock on this node.
+     */
+    public void lock() {
+        lock.lock();
+    }
+
+    /**
+     * Unlocks this node if called by the Thread owning the lock.
+     */
+    public void unlock() {
+        lock.unlock();
+    }
+
+    public boolean isLocked() {
+        return ((ReentrantLock)lock).isLocked();
+    }
+}
---
+++ concurrent_Bplus_tree/src/main/java/BplusTree/internalNode.java
@@ -0,0 +1,73 @@
+package BplusTree;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class internalNode<K extends Comparable, V> extends Node<K,V> {
+    private List<Node<K,V>> children;
+    private int numChildren;
+
+    public internalNode(K key){
+        super(key);
+        children = new ArrayList<>();
+        numChildren = 2;
+        isLeaf = false;
+    }
+
+    /** find a child that contains key
+     *
+     * @param key
+     * @return
+     */
+    public Node<K, V> getChild(K key) {
+        //TODO
+        return null;
+    }
+
+    /** add a child to the right position
+     *
+     * @param key
+     * @param addNode
+     * @return
+     */
+    public boolean addChild(K key, Node<K,V> addNode){
+        //TODO
+        return true;
+    }
+
+    /** in the case that the number of keys extends the maximum
+     *  add a child to the right position by splitting
+     *
+     * @param key
+     * @param addNode
+     * @return
+     */
+    public internalNode<K,V> splitAddChild(K key, Node<K,V> addNode){
+        //TODO
+        return null;
+    }
+
+    /** convert the internal node for visualization
+     *
+     * @return
+     */
+    public String toString() {
+        //TODO
+        return null;
+    }
+
+    public internalNode(List<Node<K,V>> children,List<K> keys,int numKeys,Node<K,V> next, Node<K,V> parent){
+        super(keys, numKeys, next, parent);
+        numChildren = numKeys + 1;
+        for(Node<K,V> node : children){
+            node.setParent(node);
+        }
+        isLeaf = false;
+    }
+
+
+
+}
